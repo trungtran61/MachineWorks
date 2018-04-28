eeba97d381f3436722e0dd007e64f694580c6b99
trungtran61 <trungtran61@gmail.com>
2018-04-27 18:16:28 -0700

    get role from DB

--- app/security/manage-user.component.ts
+++ app/security/manage-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/map';
 import { SecurityService } from './security.service';
@@ -16,19 +16,37 @@ export class ManageUserComponent implements OnInit {
   errorMessage: string;
   userId: number;
   pageTitle: string = 'New User';
+  _roles: FormArray = this.fb.array([]);
+
+  get roles(): FormArray {
+    return <FormArray>this.entryForm.get('roles');
+  }
+
+  buildRole(): FormGroup {
+    return this.fb.group({
+      role: '',
+      chkRole: ''
+    });
+  }
+
+  addRole(role: string): FormGroup {
+    return this.fb.group({
+      role: role,
+      chkRole: true
+    });
+  }
 
   constructor(private route: ActivatedRoute, private fb: FormBuilder, private secSvc: SecurityService) { }
 
   createFormGroup(): void {
-    this.entryForm = this.fb.group({     
+    this.entryForm = this.fb.group({
       userName: '',
       active: '',
       firstName: '',
       lastName: '',
       email: '',
-      address: '',
-    });   
-    
+      roles: this.fb.array([this.buildRole()])
+    });
   }
 
   ngOnInit() {
@@ -36,29 +54,58 @@ export class ManageUserComponent implements OnInit {
 
     let userId = +this.route.snapshot.params['id'];
     this.userId = userId;
-    
-    if (userId != 0)
-    {
-      this.entryForm.get('userName').disable() ;     
-      this.pageTitle = 'Manage User';
 
-      this.secSvc.getUser(userId)
-        .subscribe(user => {
-          //this.user = user;
-          this.entryForm.patchValue({
-            userName: user.UserName,
-            firstName: user.FirstName,
-            lastName: user.LastName,
-            active: user.Active,
-            email: user.Email   
-          });          
-        },
-          error => this.errorMessage = <any>error
-        );
-      }
-    else
-    {
+    if (userId != 0) {
+      this.entryForm.get('userName').disable();
+      this.pageTitle = 'Manage User';
+    }
+    else {
       this.pageTitle = 'New User';
     }
+
+    this.secSvc.getUser(userId)
+      .subscribe(
+        (user: User) => this.onUserRetrieved(user),
+        error => this.errorMessage = <any>error
+      );
+  }
+
+  onUserRetrieved(user: User): void {
+    if (this.entryForm) {
+      this.entryForm.reset();
+    }
+    this.user = user;
+    console.log(JSON.parse(user.Roles));
+    this.entryForm.patchValue({
+      userName: user.UserName,
+      firstName: user.FirstName,
+      lastName: user.LastName,
+      active: user.Active,
+      email: user.Email
+    });
+
+    let roles = JSON.parse(this.user.Roles);
+    var i: number;
+
+    for (i = 0; i < roles.length; i++) {
+      this._roles.push(this.addRole(roles[i]));
+    }
+
+    this.entryForm.setControl('roles', this._roles);
+  }
+
+  setRole(roleIndex: number)
+  {
+    console.log(this.userId);
+    var roles = this.entryForm.get('roles') as FormArray;
+    var item = roles.at(roleIndex) as FormGroup;
+    //console.log(roles);
+    //console.log(item.controls.role.value);
+    
+    this.secSvc.setUserRole(this.userId, roles.value)
+    .subscribe(
+      (user: User) => this.onUserRetrieved(user),
+      error => this.errorMessage = <any>error
+    );    
   }
 }
