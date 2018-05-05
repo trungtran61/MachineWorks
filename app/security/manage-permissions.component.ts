7dbb08e2fe2a2dc25b4578521180edea8cbd820b
trungtran61 <trungtran61@gmail.com>
2018-05-04 17:39:45 -0700

    update user status

--- app/security/manage-permissions.component.ts
+++ app/security/manage-permissions.component.ts
@@ -6,31 +6,32 @@ import { SecurityService } from './security.service';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { EditPermissionComponent } from './edit-permission.component';
 import { ModalDataService } from '../shared/modal-data.service';
+import { HandleErrorService } from '../shared/handle-error.service';
 
 @Component({
   selector: 'app-manage-permissions',
   templateUrl: './manage-permissions.component.html',
   styleUrls: ['./manage-permissions.component.css']
 })
-export class ManagePermissionsComponent implements OnInit {  
+export class ManagePermissionsComponent implements OnInit {
 
-  permissions : Permission[];
-  recordCount : number;  
+  permissions: Permission[];
+  recordCount: number;
   errorMessage: string;
   currentPage: number = 1;
   pageSize: number = environment.pageSize;
-  
-  constructor(private secSvc: SecurityService, 
-    private modalService: BsModalService, 
-    private modalDataService:ModalDataService ) { }
 
-  ngOnInit() {    
+  constructor(private secSvc: SecurityService,
+    private modalService: BsModalService,
+    private modalDataService: ModalDataService,
+    private handleErrorService: HandleErrorService) { }
+
+  ngOnInit() {
     let getListRequest: GetListRequest = new GetListRequest();
     this.getPage(1);
   }
 
-  getPage(page: number)
-  {
+  getPage(page: number) {
     this.currentPage = page;
     let getListRequest: GetListRequest = new GetListRequest();
     getListRequest.PageNumber = page;
@@ -38,24 +39,25 @@ export class ManagePermissionsComponent implements OnInit {
     this.secSvc.getPermissions(getListRequest)
       .subscribe(getPermissionsResponse => {
         this.permissions = getPermissionsResponse.Permissions;
-        this.recordCount = getPermissionsResponse.RecordCount;        
+        this.recordCount = getPermissionsResponse.RecordCount;
       },
-        error => this.errorMessage = <any>error
+        error => {
+          this.errorMessage = this.handleErrorService.handleError(error);
+        }
       );
   }
 
-  openEditPermission(id: number)
-  {
+  openEditPermission(id: number) {
     this.modalDataService.data = id;
-    const modal = this.modalService.show(EditPermissionComponent, {'class': 'modal-dialog-primary'});        
+    const modal = this.modalService.show(EditPermissionComponent, { 'class': 'modal-dialog-primary' });
   }
 
-  updatePermissionStatus(permission: Permission)
-  {
-    console.log(permission);    
+  updatePermissionStatus(permission: Permission) {
     this.secSvc.updatePermissionStatus(permission)
-      .subscribe(res => {},             
-        error => this.errorMessage = <any>error
+      .subscribe(res => { },
+        error => {
+          this.errorMessage = this.handleErrorService.handleError(error);
+        }
       );
   }
 }
