7580455aa528dbca1bfe8186b94ed0718a7fd1e4
trungtran61 <trungtran61@gmail.com>
2018-05-27 06:06:52 -0700

    add children routes for manage org

---
+++ app/organizations/manage-organization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-organization',
+  templateUrl: './manage-organization.component.html',
+  styleUrls: ['./manage-organization.component.css']
+})
+export class ManageOrganizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
